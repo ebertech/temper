fcac908ee5a96e7da618a06a755825bf63e774b7
Andrew Eberbach <andrew@ebertech.ca>
2012-05-28 15:09:14 -0400

    allow stringio's to actually set their original_filename on init

--- lib/temper.rb
+++ lib/temper.rb
@@ -74,7 +74,8 @@ module Temper
   class StringIO < Base
     def initialize(options = {})
       @storage = ::StringIO.new(options[:content].to_s)
-      super options.merge(:original_filename => "stringio.txt")
+      options[:original_filename] ||= "stringio.txt"
+      super options.merge(options)
     end
   end
 
--- lib/temper/version.rb
+++ lib/temper/version.rb
@@ -1,3 +1,3 @@
 module Temper
-  VERSION = "0.9.0"
+  VERSION = "0.9.1"
 end
